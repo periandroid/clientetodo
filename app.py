80f03cc1517b7845020019317f3b6958ccb72eff
pediandroid <joao.carlos1@escolar.ifrn.edu.br>
2024-01-05 00:43:54 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_bootstrap import Bootstrap
+import users
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+user_api = users.Users()
+
+@app.route('/')
+def index():
+    return render_template('index.html', users=user_api.list())
+
+@app.route('/user/<int:user_id>')
+def user(user_id):
+    return render_template('user.html', user_data=user_api.read(user_id))
+
+@app.route('/user/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        if not request.form['name']:
+            flash('O nome é obrigatório!')
+        elif not request.form['username']:
+            flash('O nome de usuário é obrigatório!')
+        elif not request.form['email']:
+            flash('O e-mail é obrigatório!')
+        elif not request.form['phone']:
+            flash('O telefone é obrigatório!')
+        elif not request.form['website']:
+            flash('O website é obrigatório!')
+        elif not request.form['address']:
+            flash('O endereço é obrigatório!')
+        elif not request.form['company']:
+            flash('O nome da empresa é obrigatório!')
+        else:
+            user_data = user_api.create(request.form)
+            return render_template('user.html', user_data=user_data)
+    return render_template('create.html')
+
+#Deleta mas cai o servidor antes
+@app.route('/user/<int:user_id>/delete', methods=['GET','POST'])
+def delete(user_id):
+    user_api.delete(user_id)
+    return redirect(url_for('index'))
+
+#Não faz update
+@app.route('/user/<int:user_id>/update', methods=['GET', 'POST'])
+def update(user_id):
+    user_data = user_api.read(user_id)
+    if request.method == 'POST':
+        if not request.form['name']:
+            flash('O nome é obrigatório!')
+        elif not request.form['username']:
+            flash('O nome de usuário é obrigatório!')
+        elif not request.form['email']:
+            flash('O e-mail é obrigatório!')
+        elif not request.form['phone']:
+            flash('O telefone é obrigatório!')
+        elif not request.form['website']:
+            flash('O website é obrigatório!')
+        elif not request.form['address']:
+            flash('O endereço é obrigatório!')
+        elif not request.form['company']:
+            flash('O nome da empresa é obrigatório!')
+        else:
+            user_data = user_api.update(user_id, request.form)
+            return render_template('index', user_data=user_data)
+    return render_template('update.html', user_data=user_data)
+
+#todos não funciona ainda
+@app.route('/todo/create', methods=['GET', 'POST'])
+def createTodo():
+    if request.method == 'POST':
+        if not request.form['userId']:
+            flash('O userId é obrigatório!')
+        elif not request.form['title']:
+            flash('O title é obrigatório!')
+        elif not request.form['completed']:
+            flash('Informe o completed')
+        else:
+            user_data = user_api.create(request.form)
+            return render_template('user.html', user_data=user_data)
+    return render_template('todo.html')
+if __name__ == '__main__':
+    app.run(debug=True)
